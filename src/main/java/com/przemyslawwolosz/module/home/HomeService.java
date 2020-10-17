6b9fa728863e9af6807ebeaa2839f31ba2e62455
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-10-17 10:09:17 +0200

    Add module home

---
+++ src/main/java/com/przemyslawwolosz/module/home/HomeService.java
@@ -0,0 +1,11 @@
+package com.przemyslawwolosz.module.home;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class HomeService {
+    public String getGreetings() {
+        return "Hello world";
+    }
+}
